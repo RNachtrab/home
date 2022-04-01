e5299bdffe07d8cac384328794235bc6bc59e9ab
RNachtrab <102812369+RNachtrab@users.noreply.github.com>
2022-04-01 16:33:35 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/RNachtrab/home";
+const repoURL = "https://github.com/RNachtrab/RNachtrab.github.io.git";
 
 ghpages.publish(
   pathname,
